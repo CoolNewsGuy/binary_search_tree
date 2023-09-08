86ec5729a48237747eb6f4a9ed1e8b525918907a
Aymen <joewilliamdd31@gmail.com>
2023-09-08 11:40:34 +0100

    Build BST from an array

--- main.ts
+++ main.ts
@@ -13,8 +13,36 @@ class Node {
 class BinarySearchTree {
     root: Node | null;
 
-    constructor(root: Node | null = null) {
-        this.root = root;
+    constructor(arr?: number[]) {
+        this.root = arr === undefined ? null : this.buildFromArray(arr);
+    }
+
+    private buildFromArray(
+        arr: number[],
+        start = 0,
+        end?: number
+    ): Node | null {
+        if (arr.length === 0) {
+            return null;
+        }
+
+        // sort the array and set end
+        if (end === undefined) {
+            arr = [...new Set(arr)];
+            arr.sort();
+            end = arr.length - 1;
+        }
+
+        if (start > end) {
+            return null;
+        }
+
+        const middle = Math.floor((start + end) / 2);
+        const root = new Node(arr[middle]);
+
+        root.left = this.buildFromArray(arr, start, middle - 1);
+        root.right = this.buildFromArray(arr, middle + 1, end);
+
+        return root;
     }
 }
-
