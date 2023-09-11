005bfe32d4d037f3162427e12ea88c3565448e56
Aymen <joewilliamdd31@gmail.com>
2023-09-11 22:18:21 +0100

    Add more elements to the tree

--- example.ts
+++ example.ts
@@ -22,7 +22,7 @@ function generateRandomNumbers(
     return numbers;
 }
 
-const numbers = generateRandomNumbers(0, 100, 20);
+let numbers = generateRandomNumbers(0, 100, 20);
 const tree = new BinarySearchTree(numbers);
 
 tree.prettyPrint();
@@ -32,3 +32,6 @@ console.log("The elements in level-order: ", tree.levelOrder());
 console.log("The elements in pre-order: ", tree.preoder());
 console.log("The elements in post-order: ", tree.postorder());
 console.log("The elements in in-order: ", tree.inorder());
+
+numbers = generateRandomNumbers(100, 500, 20);
+numbers.forEach((num) => tree.insert(num));
