acb5afb448451132ccce09d0467d3fc931ea1ca1
Aymen <joewilliamdd31@gmail.com>
2023-09-11 22:24:10 +0100

    Print node values in different orders

--- example.ts
+++ example.ts
@@ -40,4 +40,9 @@ tree.prettyPrint();
 console.log("Balanced tree after adding elements?", tree.isBalanced());
 tree.rebalance();
 tree.prettyPrint();
+
 console.log("Balanced tree after adding rebalancing?", tree.isBalanced());
+console.log("The elements in level-order: ", tree.levelOrder());
+console.log("The elements in pre-order: ", tree.preoder());
+console.log("The elements in post-order: ", tree.postorder());
+console.log("The elements in in-order: ", tree.inorder());
